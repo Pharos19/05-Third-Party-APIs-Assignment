56893323b65f50528aed9c3541853b742fed6b7a
Pharos19 <Pharos199@gmail.com>
2022-10-03 21:41:36 -0500

    Added script.js

---
+++ Develop/script.js
@@ -0,0 +1,10 @@
+//current day displayed in header using id=currentDay.
+
+// each time block is color coded to show if it is past(grey), present(red), future(green).
+
+//example: current time is red(present), if current time is 11am everything before 11am is grey(past). else everything after 11am is green(future).
+
+// when clicking on time block enter inpur from user.
+// save input to local storage when save button is clicked(when refreshed info is not erased).
+
+//get items from local storage and set them to variables
