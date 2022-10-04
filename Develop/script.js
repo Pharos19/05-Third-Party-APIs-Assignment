185ed4bba219d9648183cc24dbcc5053ebf1b9c3
Pharos19 <Pharos199@gmail.com>
2022-10-03 21:51:06 -0500

    Working in JS, adding code

--- Develop/script.js
+++ Develop/script.js
@@ -7,4 +7,14 @@
 // when clicking on time block enter inpur from user.
 // save input to local storage when save button is clicked(when refreshed info is not erased).
 
+
 //get items from local storage and set them to variables
+var textareaVal9 = localStorage.getItem("hr-9");
+var textareaVal10 = localStorage.getItem("hr-10");
+var textareaVal11 = localStorage.getItem("hr-11");
+var textareaVal12 = localStorage.getItem("hr-12");
+var textareaVal1 = localStorage.getItem("hr-1");
+var textareaVal2 = localStorage.getItem("hr-2");
+var textareaVal3 = localStorage.getItem("hr-3");
+var textareaVal4 = localStorage.getItem("hr-4");
+var textareaVal5 = localStorage.getItem("hr-5");
